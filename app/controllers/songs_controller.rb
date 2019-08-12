fbfeb4e9dbc96b6c0f3228e07681939fff836825
Tony Baker <this.tonybaker@gmail.com>
2019-08-12 19:19:35 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,10 @@
 class SongsController < ApplicationController
   def index
+    if params[:artist_id]
+      @songs = Artist.find(params[artist_id]).songs
+    else
     @songs = Song.all
+    end
   end
 
   def show
