bc498133a11caf551c9dc8656c776bb594f1578c
Tony Baker <this.tonybaker@gmail.com>
2019-08-12 19:45:33 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      artist = Artist.find(params[:artist_id])
+      artist = Artist.find_by(id: params[:artist_id])
       if artist.nil?
         flash[:alert] = "Artist not found."
         redirect_to artists_path
